30f553f29adf686f0407dc90942f8f7c2a84b954
irek <mail@gumen.pl>
2022-05-07 16:31:18 +0200

    Cleanup and document

--- main.c
+++ main.c
@@ -4,10 +4,13 @@
 #include <errno.h>
 #include <stdarg.h>
 
+/* For creating socket connection. */
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+
+#define PORT	"1965"		/* Default Gemini port */
 
 void
 die(char *fmt, ...)
@@ -27,48 +30,51 @@ die(char *fmt, ...)
 	exit(1);
 }
 
-int
+/* Open socket to Gemini capsule with given URL (without gemini://
+ * protocol prefix). */
+int				/* Return Scoket File Descriptor */
 gmi_connect(char *url)
 {
 	int     sfd;		/* Socket file descriptor */
-	int     err;		/* For error codes */
+	int     err;		/* For error code */
 
-	struct addrinfo  hint;	/* To hint connection information */
-	struct addrinfo *res;   /* Result of getaddrinfo */
-	struct addrinfo *rp;	/* Result pointer */
+	/* HINT hints getaddrinfo that given URL is for IP TCP socket
+	 * connection.  RES points at list of getaddrinfo results and
+	 * RP (Results Pointer) is used to iterate over them. */
+	struct addrinfo hint, *res, *rp;
 
-	memset(&hint, 0, sizeof(hint)); /* Init with 0 */
+	memset(&hint, 0, sizeof(hint));	/* Init with 0 */
 
-	hint.ai_family   = AF_INET;	/* Internet Address Family */
-	hint.ai_socktype = SOCK_STREAM; /* Two-way stream connection */
+	/* Combination of AF_INET, SOCK_STREAM and ai_protocol (as 0,
+	 * because structure is init with 0) makes it IP TCP socket.
+	 * Read ip(7) and tcp(7) man pages for more information.
+	 * Other possible values are listed in socket(2) man page. */
 
-	/* Combination of AF_INET and SOCK_STREAM and with ai_protocol
-	 * value set to 0 (default because whole structure is init
-	 * with 0) it makes it IP TCP socket.  For more informations
-	 * read ip(7) and tcp(7) man pages. */
+	hint.ai_family   = AF_INET;
+	hint.ai_socktype = SOCK_STREAM;
 
-	/* 1965 is hardcoded default gemini port */
-	if ((err = getaddrinfo(url, "1965", &hint, &res)) != 0)
+	/* TODO(irek): For now default Gemini protocol is used but
+	 * Gemini allows for protocol to be defined in URL so it can
+	 * be different for each connection. */
+
+	if ((err = getaddrinfo(url, PORT, &hint, &res)) != 0)
 		die("getaddrinfo: %s", gai_strerror(err));
 
 	for (rp = res; rp != NULL; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family,
-			     rp->ai_socktype,
-			     rp->ai_protocol);
-
-		if (sfd == -1)
-			continue;
+		if ((sfd = socket(rp->ai_family,
+				  rp->ai_socktype,
+				  rp->ai_protocol)) == -1)
+			continue; /* Failure */
 
 		if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
 			break;	/* Success */
 
 		close(sfd);
 	}
-
 	freeaddrinfo(res);	/* No longer needed */
 
 	if (rp == NULL)
-		die("Could not connect");
+		die("Can't connect to %s", url);
 
 	return sfd;
 }
