86aa29d05e84b189bcb529ac626090b7cd1ed008
irek <irek@gabr.pl>
2022-06-21 09:39:54 +0200

    Add more test for str lib

--- str.t.c
+++ str.t.c
@@ -9,5 +9,12 @@ TEST("strnrep")
 
 	OK(strnrep("str ing with spa ces", " ", "%20", buf, BSIZ) == 0);
 	BUF_EQ(buf, "str%20ing%20with%20spa%20ces", BSIZ);
+
+	OK(strnrep("beg AAA middle aaa AAA end", "AAA", "TEST", buf, BSIZ) == 0);
+	BUF_EQ(buf, "beg TEST middle aaa TEST end", BSIZ);
+
+	OK(strnrep("beg AAA middle aaa AAA end", "BBB", "TEST", buf, BSIZ) == 0);
+	BUF_EQ(buf, "beg AAA middle aaa AAA end", BSIZ);
+
 	OK(strnrep("str ing with spa ces", " ", "%20", buf, 10) != 0);
 }
